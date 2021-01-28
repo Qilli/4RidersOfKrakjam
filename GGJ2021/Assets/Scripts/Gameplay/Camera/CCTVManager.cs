29c165b15faead14871208f7fd8dcdbad91fe0b6
dnowakowski <dnowakowski404@gmail.com>
2021-01-28 23:57:47 +0100

    scenki, eventy na lose & win, base scrollowanie i poruszanie sie wsadem

---
+++ GGJ2021/Assets/Scripts/Gameplay/Camera/CCTVManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Klasa odpowiadająca za obsługiwanie kamer CCTV (Gameplay) - tj zmiana
+/// </summary>
+
+public class CCTVManager : MonoBehaviour
+{
+    [SerializeField]
+    Camera gameCamera;
+
+    public List<CCTVCamera> cameras = new List<CCTVCamera>();
+
+    public void moveGameCameraTo(int index)
+    {
+        gameCamera.transform.position = cameras[index].transform.position;
+    }
+
+    public void moveGameCameraTo(CCTVCamera cam)
+    {
+        int index = cameras.FindIndex(x => x == cam);
+        moveGameCameraTo(index);
+    }
+
+}
---
+++ GGJ2021/Assets/Scripts/Gameplay/GameplayLoopEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[CreateAssetMenu(menuName ="Gameplay/GameplayLoopEvent")]
+public class GameplayLoopEvent : ScriptableObject
+{
+    UnityEvent desiredEvent;
+    /// <summary>
+    /// Pole odpowiadające za wywoływanie eventu
+    /// </summary>
+    [SerializeField]
+    bool Enabled = true;
+
+    public void Invoke()
+    {
+        if(Enabled)
+        desiredEvent.Invoke();
+    }
+
+    public void Register(UnityAction action )
+    {
+        desiredEvent.AddListener(action);
+    }
+
+    public void UnRegister(UnityAction action)
+    {
+        desiredEvent.RemoveListener(action);
+    }
+
+}
