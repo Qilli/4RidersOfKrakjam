cf495e97e88f6bdde8ce65aaa3cb808aa92cbff9
Berzyliusz <k.poros@gmail.com>
2021-01-29 20:16:08 +0100

    Working on randomizing

---
+++ GGJ2021/Assets/Scripts/BodyParts Randomizing/BodyPart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPart : MonoBehaviour
+{
+    public enum BodyPartType {None, HairMale, HairFemale, Trousers, FemaleHead, MaleHead, SkinPart, Shirt, Luggage, Backpack, EyesFemale, EyesMale, ShirtDecor}
+
+    [SerializeField] BodyPartType _partType;
+
+    public BodyPartType Type { get { return _partType; } }
+}
--- GGJ2021/Assets/Scripts/PersonsRandomizer.cs
+++ GGJ2021/Assets/Scripts/PersonsRandomizer.cs
@@ -7,72 +7,100 @@ using UnityEngine;
 /// </summary>
 public class PersonsRandomizer : MonoBehaviour
 {
-    [Header("Clothes")]
-    [SerializeField] List<GameObject> _trousers = new List<GameObject>();
-    [SerializeField] List<GameObject> _shirts = new List<GameObject>();
-    [SerializeField] List<GameObject> _shirtDecor = new List<GameObject>();
-    [SerializeField] List<GameObject> _glasses = new List<GameObject>();
-    [SerializeField] List<GameObject> _inHandDecors = new List<GameObject>();
-    [SerializeField] List<GameObject> _neckDecor = new List<GameObject>();
-    [SerializeField] List<GameObject> _headDecor = new List<GameObject>();
+    [SerializeField] List<Color> _clothsColor = new List<Color>();
+    [SerializeField] List<Color> _skinTones = new List<Color>();
+    [SerializeField] List<Color> _hairColors = new List<Color>();
 
-    [Header("Body")]
-    [SerializeField] List<GameObject> _skinTone = new List<GameObject>();
-    [SerializeField] List<GameObject> _leftHandTatoo = new List<GameObject>();
-    [SerializeField] List<GameObject> _rightHandTatoo = new List<GameObject>();
-    [SerializeField] List<GameObject> _hairAndBeard = new List<GameObject>();
+    // Serialize chances
 
     public void RandomizePerson(Person person)
     {
-        List<GameObject> newElements = new List<GameObject>();
+        var female = UnityEngine.Random.Range(0, 100) > 50;
+        var useLuggage = UnityEngine.Random.Range(0, 100) > 50;
+        var useBackpack = UnityEngine.Random.Range(0, 100) > 50;
 
-        newElements.AddRange(RandomizeClothes());
+        var components = person.gameObject.GetComponentsInChildren<BodyPart>();
 
-        if (!person.IsPrisoner)
+        var trousersColor = _clothsColor[UnityEngine.Random.Range(0, _clothsColor.Count)];
+        var skinTone = _skinTones[UnityEngine.Random.Range(0, _skinTones.Count)];
+        var hairColor = _hairColors[UnityEngine.Random.Range(0, _hairColors.Count)];
+        var shirtColor = _clothsColor[UnityEngine.Random.Range(0, _clothsColor.Count)];
+        var backpackColor = _clothsColor[UnityEngine.Random.Range(0, _clothsColor.Count)];
+
+        List<BodyPart> hairs = new List<BodyPart>();
+
+        foreach (var bp in components)
         {
-            newElements.AddRange(RandomizeBody());
+            bp.gameObject.SetActive(false);
+
+            if(bp.Type == BodyPart.BodyPartType.Trousers)
+            {
+                bp.gameObject.SetActive(true);
+                bp.GetComponent<SpriteRenderer>().color = trousersColor;
+            }
+
+            if(bp.Type == BodyPart.BodyPartType.FemaleHead && female)
+            {
+                bp.gameObject.SetActive(true);
+                bp.GetComponent<SpriteRenderer>().color = skinTone;
+            }
+
+            if(bp.Type == BodyPart.BodyPartType.MaleHead && !female)
+            {
+                bp.gameObject.SetActive(true);
+                bp.GetComponent<SpriteRenderer>().color = skinTone;
+            }
+
+            if (bp.Type == BodyPart.BodyPartType.SkinPart)
+            {
+                bp.gameObject.SetActive(true);
+                bp.GetComponent<SpriteRenderer>().color = skinTone;
+            }
+
+            if(bp.Type == BodyPart.BodyPartType.HairFemale && female)
+            {
+                //Debug.Log("Adding hair female");
+                hairs.Add(bp);
+            }
+
+            if(bp.Type == BodyPart.BodyPartType.HairMale && !female)
+            {
+                //Debug.Log("Adding hair male");
+                hairs.Add(bp);
+            }
+
+            if(bp.Type == BodyPart.BodyPartType.Shirt)
+            {
+                bp.gameObject.SetActive(true);
+                bp.GetComponent<SpriteRenderer>().color = shirtColor;
+            }
+
+            if(bp.Type == BodyPart.BodyPartType.Luggage && useLuggage)
+            {
+                bp.gameObject.SetActive(true);
+            }
+
+            if(bp.Type == BodyPart.BodyPartType.Backpack && useBackpack)
+            {
+                bp.gameObject.SetActive(true);
+                bp.GetComponent<SpriteRenderer>().color = backpackColor;
+            }
+
+            if(bp.Type == BodyPart.BodyPartType.EyesFemale && female)
+            {
+                bp.gameObject.SetActive(true);
+            }
+
+            if (bp.Type == BodyPart.BodyPartType.EyesMale && !female)
+            {
+                bp.gameObject.SetActive(true);
+            }
+
         }
-        else
-        {
-            newElements.AddRange(person.PrisonerReference.GetPrisonerElements());
-        }
 
-        person.SetPersonLooks(newElements);
+        var hair = hairs[UnityEngine.Random.Range(0, hairs.Count)];
+        hair.GetComponent<SpriteRenderer>().color = hairColor;
+        hair.gameObject.SetActive(true);
     }
 
-    private List<GameObject> RandomizeBody()
-    {
-        List<GameObject> elements = new List<GameObject>();
-
-        elements.Add(ChooseRandomOf(_skinTone));
-        elements.Add(ChooseRandomOf(_leftHandTatoo));
-        elements.Add(ChooseRandomOf(_rightHandTatoo));
-        elements.Add(ChooseRandomOf(_hairAndBeard));
-        //Debug.Log("Body randomized");
-
-        return elements;
-    }
-
-    private List<GameObject> RandomizeClothes()
-    {
-        List<GameObject> elements = new List<GameObject>();
-
-        elements.Add(ChooseRandomOf(_trousers));
-        elements.Add(ChooseRandomOf(_shirts));
-        elements.Add(ChooseRandomOf(_shirtDecor));
-        elements.Add(ChooseRandomOf(_glasses));
-        elements.Add(ChooseRandomOf(_inHandDecors));
-        elements.Add(ChooseRandomOf(_neckDecor));
-        elements.Add(ChooseRandomOf(_headDecor));
-        //Debug.Log("Clothes randomized");
-
-        return elements;
-    }
-
-    private GameObject ChooseRandomOf(List<GameObject> elementsToChoose)
-    {
-        if (elementsToChoose == null || elementsToChoose.Count == 0) return null;
-
-        return elementsToChoose[UnityEngine.Random.Range(0, elementsToChoose.Count)];
-    }
 }
